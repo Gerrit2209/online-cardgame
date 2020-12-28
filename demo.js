44462eb056e9c7da7538e9f23bed7866e0c71097
Gerrit Pommerehne <Gerrit.Pommerehne@gmx.de>
2020-12-28 03:11:39 +0100

    v4

--- demo.js
+++ demo.js
@@ -1,8 +1,11 @@
-var http = require("http");
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(req.url);
-    res.end();
-  })
-  .listen(5000);
+const http = require("http");
+const host = "http://ec2-18-193-150-60.eu-central-1.compute.amazonaws.com";
+const port = 8000;
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end("My first server!");
+};
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
